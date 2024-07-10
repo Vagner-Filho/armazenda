7d0aeb6e7a28a8e86427fd380d259883cb04d2a8
Vagner Filho <vagner.filhopro@gmail.com>
2024-07-09 22:16:45 -0400

    embedding

--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"armazenda/model/grain_model"
 	"armazenda/router/grain_router"
+	"embed"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
+//go:embed templates/*
+var templatesFS embed.FS
+
+//go:embed assets
+var assetsFS embed.FS
+
 func main() {
 	grain_model.InitGrainMap()
 	router := gin.Default()
-	html := template.Must(template.ParseGlob("templates/*"))
+	html := template.Must(template.ParseFS(templatesFS, "templates/*"))
 	router.SetHTMLTemplate(html)
 
-	router.Static("/assets/static/css", "./assets/static/css")
-	router.LoadHTMLGlob("templates/*")
+    router.StaticFS("/public", http.FS(assetsFS))
+	// router.Static("/assets/static/css", "./assets/static/css")
+	// router.LoadHTMLGlob("templates/*")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "home", gin.H{})
 	})
@@ -28,5 +38,17 @@ func main() {
 	})
 
 	router.POST("/grao", grain_router.AddGrain)
-	router.Run(":3000")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8100"
+	}
+
+    ipv6Addr := "::" // Listen on all IPv6 addresses
+	if envIP := os.Getenv("IP"); envIP != "" {
+		ipv6Addr = envIP
+	}
+
+    address := fmt.Sprintf("[%s]:%s", ipv6Addr, port)
+	router.Run(address)
 }
