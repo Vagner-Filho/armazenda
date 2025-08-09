d9b8c05f577af7815f96421db670ea2f0dfe0b81
Vagner Filho <vagner.filhopro@gmail.com>
2025-08-08 21:47:00 -0400

    feat: remove empty fields from PUT farm config request

--- entity/public/address.go
+++ entity/public/address.go
@@ -2,13 +2,13 @@ package entity_public
 
 type Address struct {
 	Id           *uint16 `form:"id"`
-	Street       *string `form:"street" binding:"required"`
-	Cep          *string `form:"cep" binding:"required"`
+	Street       *string `form:"street"`
+	Cep          *string `form:"cep"`
 	Number       *uint32 `form:"number"`
 	Complement   *string `form:"complement"`
-	Neighborhood *string `form:"neighborhood" binding:"required"`
-	City         *string `form:"city" binding:"required"`
-	State        *string `form:"state" binding:"required"`
+	Neighborhood *string `form:"neighborhood"`
+	City         *string `form:"city"`
+	State        *string `form:"state"`
 	Email        *string `form:"email"`
 	PhoneNumber  *string `form:"phoneNumber"`
 }
